74cee21e5dbbfdbe5aa59d50ecf9dbf940b8eebf
ESHKEREEEEEE <trishechkin.sasha@yandex.ru>
2025-07-23 16:55:19 +0700

    Added shell commands

--- sched--1.0.sql
+++ sched--1.0.sql
@@ -5,29 +5,35 @@
 
 CREATE SCHEMA IF NOT EXISTS sched;
 
+--Table for tasks
 CREATE TABLE sched.tasks (
     id serial PRIMARY KEY,
     command text NOT NULL,
     run_at timestamp NOT NULL,
+    type text NOT NULL,
     created_at timestamp DEFAULT now(),
     status text DEFAULT 'pending',
     executed_at timestamp,
     error text
 );
 
---added as sql function for testing
-create function call_shell_command(cmd text)
-RETURNS integer
-AS 'sched', 'call_shell_command'
-LANGUAGE C STRICT;
+--Table for permitted shell commands
+CREATE TABLE sched.whitelist (
+    id serial PRIMARY KEY,
+    command text NOT NULL
+);
 
-CREATE FUNCTION exec_sql(cmd text)
-RETURNS integer
-AS 'sched', 'exec_sql'
-LANGUAGE C STRICT;
+--Test values for whitelist
+INSERT INTO sched.whitelist(command) VALUES ('ls'); 
+
+--added as sql function for testing
+--create function call_shell_command(cmd text)
+--RETURNS integer
+--AS 'sched', 'call_shell_command'
+--LANGUAGE C STRICT;
 
 --Task scheduling function
-CREATE FUNCTION schedule_task(cmd text, run_at timestamptz)
+CREATE FUNCTION schedule_task(cmd text, run_at timestamptz, type text)
 RETURNS void
 AS 'sched', 'schedule_task'
 LANGUAGE C STRICT;
--- sched.c
+++ sched.c
@@ -8,14 +8,16 @@
 #include "utils/lsyscache.h"
 #include "utils/elog.h"
 #include "sched.h"
+#include <sys/wait.h>
 #include <stdlib.h>
 
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(call_shell_command);
-PG_FUNCTION_INFO_V1(exec_sql);
 PG_FUNCTION_INFO_V1(schedule_task);
 PG_FUNCTION_INFO_V1(run_due_tasks);
+
+bool exec_sql(char* sql);
+int call_shell_command(char* cmd);
 
 volatile sig_atomic_t got_sigterm = false;
 
@@ -27,51 +29,84 @@ void die(SIGNAL_ARGS)
 typedef struct {
     int id;
     char *cmd;
+    char *type;
 } TaskInfo;
 
-Datum
-call_shell_command(PG_FUNCTION_ARGS)
+typedef struct {
+	int id;
+	char *cmd;
+} WhitelistInfo;
+
+int call_shell_command(char* cmd)
 {
-    text *cmd_text = PG_GETARG_TEXT_PP(0);
+	int ret;
+	int nrows;
+	int spi_status;
+	HeapTuple tuple;
+    TupleDesc tupdesc;
+    Datum cmd_datum;
+    bool isnull;
+    uint64 i;
+	
+    ret = -1;
     
-    //TODO: Add whitelist of commands for protection ot che popalo
+    spi_status = SPI_connect();
+    if (spi_status != SPI_OK_CONNECT)
+        elog(ERROR, "SPI_connect failed");
+
+	//Getting commands from whitelist
+    elog(LOG, "[sched:call_shell_command()] Getting whitelist...");
+    spi_status = SPI_execute("SELECT id, command FROM sched.whitelist ", false, 0);
+    elog(LOG, "[sched:call_shell_command()] SPI_execute status: %d", spi_status);
+
+    if (spi_status != SPI_OK_SELECT)
+        elog(ERROR, "SPI_execute failed when selecting tasks");
+
+    if (SPI_tuptable == NULL){ elog(ERROR, "SPI_tuptable is NULL"); }
+	
+	nrows = SPI_processed;
+    tupdesc = SPI_tuptable->tupdesc;
+	
+    for (i = 0; i < nrows; i++)
+    {
+        tuple = SPI_tuptable->vals[i];
+        cmd_datum = SPI_getbinval(tuple, tupdesc, 2, &isnull);
+        if (isnull)
+            continue;
+
+        if (!strcmp(cmd, TextDatumGetCString(cmd_datum))) {
+        ret = system(cmd); //Call
+        
+        elog(LOG, "[sched:call_shell_command()] Executed shell command '%s' with result %d", cmd, ret);
+        break;
+        };
+    }
     
-    char *cmd = text_to_cstring(cmd_text);
-
-    int ret = system(cmd);  // Call
-
-    PG_RETURN_INT32(ret);
+	SPI_finish();
+    return ret;
 }
 
-Datum
-exec_sql(PG_FUNCTION_ARGS)
-{
-    text *sql_text = PG_GETARG_TEXT_PP(0);
-    char *sql = text_to_cstring(sql_text);
 
+bool exec_sql(char* sql)
+{
     int ret;
     int spi_status;
-    int rows;
-
+    
     spi_status = SPI_connect();
     if (spi_status != SPI_OK_CONNECT)
         elog(ERROR, "SPI_connect failed: %d", spi_status);
 
     ret = SPI_execute(sql, false, 0);  // false = read-only off, 0 = no limit
 
-    if (ret != SPI_OK_SELECT && ret != SPI_OK_INSERT &&
-        ret != SPI_OK_UPDATE && ret != SPI_OK_DELETE &&
-        ret != SPI_OK_INSERT_RETURNING && ret != SPI_OK_UPDATE_RETURNING &&
-        ret != SPI_OK_DELETE_RETURNING && ret != SPI_OK_UTILITY)
+    if (ret < 0) //Unsuccessful
     {
         SPI_finish();
         elog(ERROR, "SPI_execute failed: %d", ret);
+        return false;
     }
 
-    rows = SPI_processed;
     SPI_finish();
-
-    PG_RETURN_INT32(rows);
+	return true;
 }
 
 Datum
@@ -79,8 +114,9 @@ schedule_task(PG_FUNCTION_ARGS)
 {
     text *cmd_text = PG_GETARG_TEXT_PP(0);
     TimestampTz run_at = PG_GETARG_TIMESTAMPTZ(1);
+    text *type_text = PG_GETARG_TEXT_PP(2);
     char *cmd = text_to_cstring(cmd_text);
-
+	char *type = text_to_cstring(type_text);
     int ret;
     char sql[1024];
 
@@ -89,8 +125,8 @@ schedule_task(PG_FUNCTION_ARGS)
 
 	//Creating sql
     snprintf(sql, sizeof(sql),
-             "INSERT INTO sched.tasks(command, run_at) VALUES('%s', '%s')",
-             cmd, run_at_str);
+             "INSERT INTO sched.tasks(command, run_at, type) VALUES('%s', '%s', '%s')",
+             cmd, run_at_str, type);
 
     ret = SPI_connect();
     if (ret != SPI_OK_CONNECT)
@@ -118,13 +154,19 @@ run_due_tasks(PG_FUNCTION_ARGS)
     TupleDesc tupdesc;
     Datum id_datum;
     Datum cmd_datum;
+    Datum type_datum;
     bool isnull;
     char update_sql[1024];
-    int exec_ret;
 
     TaskInfo *tasks = NULL;
     int id; 
     char *cmd;
+    char *type;
+    char *status_str;
+    char *error_str;
+    int ret = -1;
+    
+    bool shell_err;
 
     PG_TRY();
     {
@@ -137,7 +179,7 @@ run_due_tasks(PG_FUNCTION_ARGS)
 		//Getting tasks from sched.tasks
         elog(LOG, "[sched:run_due_tasks()] Selecting due tasks...");
         spi_status = SPI_execute(
-            "SELECT id, command FROM sched.tasks "
+            "SELECT id, command, type FROM sched.tasks "
             "WHERE run_at <= now() AND status = 'pending'",
             false, 0);
 
@@ -167,9 +209,14 @@ run_due_tasks(PG_FUNCTION_ARGS)
             cmd_datum = SPI_getbinval(tuple, tupdesc, 2, &isnull);
             if (isnull)
                 continue;
-
+            type_datum = SPI_getbinval(tuple, tupdesc, 3, &isnull);
+			if (isnull)
+				continue;
+				
             tasks[i].id = DatumGetInt32(id_datum);
             tasks[i].cmd = pstrdup(TextDatumGetCString(cmd_datum)); 
+            tasks[i].type = pstrdup(TextDatumGetCString(type_datum)); 
+            elog(LOG, "[sched:run_due_tasks()] Added id: %d, %s with type %s", tasks[i].id, tasks[i].cmd, tasks[i].type);
         }
 
         // Running tasks from TaskInfo
@@ -177,32 +224,79 @@ run_due_tasks(PG_FUNCTION_ARGS)
         {
             id = tasks[i].id;
             cmd = tasks[i].cmd;
-			elog(LOG, "[sched] Running task id %d: %s", id, cmd);
-            if (cmd == NULL || strlen(cmd) == 0)
+            type = tasks[i].type;
+            
+            
+			elog(LOG, "[sched] Running task id %d: %s with type: %s", id, cmd, type);
+            if (cmd == NULL || strlen(cmd) == 0 || type == NULL || strlen(type) == 0)
             {
             	elog(LOG, "[sched] Found NULL");
                 continue;
 			}
 			
-			//TODO: Add shell commands with call_shell_command()
-			
-			
-            exec_ret = SPI_execute(cmd, false, 0);
-
-            if (exec_ret == SPI_OK_SELECT || exec_ret == SPI_OK_INSERT ||
-                exec_ret == SPI_OK_UPDATE || exec_ret == SPI_OK_DELETE ||
-                exec_ret == SPI_OK_INSERT_RETURNING || exec_ret == SPI_OK_UPDATE_RETURNING ||
-                exec_ret == SPI_OK_DELETE_RETURNING || exec_ret == SPI_OK_UTILITY)
-            {
+			if (!strcmp(type, "sh"))	//type == sh => shell
+			{
+				shell_err = 0;
+				
+				ret = call_shell_command(cmd);
+				
+				elog(LOG, "[sched:run_due_tasks()] Found command with sh type");
+				
+				
+        		if (ret == -1) 
+        		{
+    				status_str = "failed";
+    				error_str = "system() call failed";
+    				shell_err = 1;
+				} 
+				else if (WIFEXITED(ret)) 
+				{
+    				int exit_code = WEXITSTATUS(ret);
+    				if (exit_code == 0) 
+        				status_str = "done";
+    				else 
+    				{
+        				status_str = "failed";
+        				error_str = psprintf("exit code %d", exit_code);
+        				shell_err = 1;
+    				}
+				} 
+				else if (WIFSIGNALED(ret)) 
+				{
+    				status_str = "failed";
+    				error_str = psprintf("terminated by signal %d", WTERMSIG(ret));
+    				shell_err = 1;
+				} else 
+				{
+    				status_str = "failed";
+    				error_str = "unknown error";
+    				shell_err = 1;
+				}
+        
+				if (shell_err)
+				{
+				snprintf(update_sql, sizeof(update_sql),
+                         	"UPDATE sched.tasks SET status = '%s', executed_at = now(), error = '%s' WHERE id = %d", status_str, error_str, id);
+                } 
+                else 
+                {
                 snprintf(update_sql, sizeof(update_sql),
-                         "UPDATE sched.tasks SET status = 'done', executed_at = now() WHERE id = %d", id);
-                elog(LOG, "[sched] Task %d executed successfully", id);
-            }
-            else
-            {
-                elog(WARNING, "[sched] Task %d failed to execute: %s", id, cmd);
-                snprintf(update_sql, sizeof(update_sql),
-                         "UPDATE sched.tasks SET status = 'failed', executed_at = now(), error = 'execution error' WHERE id = %d", id);
+                         	"UPDATE sched.tasks SET status = '%s', executed_at = now() WHERE id = %d", status_str, id);
+                }
+			}
+			else {		//type != sh => sql
+            	if (exec_sql(cmd)) //Using exec_sql to run sql tasks
+            	{
+                	snprintf(update_sql, sizeof(update_sql),
+                         	"UPDATE sched.tasks SET status = 'done', executed_at = now() WHERE id = %d", id);
+                	elog(LOG, "[sched] Task %d executed successfully", id);
+            	}
+            	else
+            	{
+                	elog(WARNING, "[sched] Task %d failed to execute: %s", id, cmd);
+                	snprintf(update_sql, sizeof(update_sql),
+                         	"UPDATE sched.tasks SET status = 'failed', executed_at = now(), error = 'execution error' WHERE id = %d", id);
+            	}
             }
             
 			//Updating current task status
